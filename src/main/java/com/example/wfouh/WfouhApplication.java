ceda724167ad66bde79f7d7b5ef3ee0ad552cd1b
AKPatrashkov18 <AKPatrashkov18@codingburgas.bg>
2022-11-15 15:34:46 +0200

    yes

---
+++ src/main/java/com/example/wfouh/WfouhApplication.java
@@ -0,0 +1,13 @@
+package com.example.wfouh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfouhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WfouhApplication.class, args);
+    }
+
+}
